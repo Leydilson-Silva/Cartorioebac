5ea7f978e1b2b82a1799f48789cd291afedd925e
Leydilson Silva <leydilson.94@gmail.com>
2024-10-29 10:14:33 -0300

    adicionado opção de sair
    Foi adicionado a opção de sair para o usuário.

--- cartorio.c
+++ cartorio.c
@@ -126,7 +126,8 @@ int main()
         printf("Escolha a opção desejada:\n\n");
         printf("\t1 - Registrar nomes.\n");
         printf("\t2 - Consultar nomes.\n");
-        printf("\t3 - Deletar nomes.\n\n");
+        printf("\t3 - Deletar nomes.\n");
+        printf("\t4 - Sair do programa.\n\n");
         printf("Opção: "); // fim menu
 
         scanf("%d", &opcao); // armazenando a escolha do usuário
@@ -145,6 +146,11 @@ int main()
 
         case 3:
             deletar();
+            break;
+            
+        case 4:
+            printf("Obrigado por utilizar o sistema!\n");
+            return 0;
             break;
 
         default:
